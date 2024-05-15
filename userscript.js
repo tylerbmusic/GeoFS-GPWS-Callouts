fcdbfd91e57b1fca3f4be2b5a21b2dddd9f72e8b
tylerbmusic <126616809+tylerbmusic@users.noreply.github.com>
2024-05-15 18:01:21 -0500

    Create userscript.js
    v0.6

---
+++ userscript.js
@@ -0,0 +1,235 @@
+// ==UserScript==
+// @name         GeoFS GPWS callouts
+// @version      0.6
+// @description  Adds some GPWS callouts
+// @author       GGamerGGuy
+// @match        https://www.geo-fs.com/geofs.php?v=*
+// @match        https://*.geo-fs.com/geofs.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+    window.a2500 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/2500.wav'); //✓
+    window.a2000 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/2000.wav'); //✓
+    window.a1000 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/1000.wav'); //✓
+    window.a500 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/500.wav'); //✓
+    window.a400 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/400.wav'); //✓
+    window.a300 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/300.wav'); //✓
+    window.a200 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/200.wav'); //✓
+    window.a100 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/100.wav'); //✓
+    window.a50 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/50.wav'); //✓
+    window.a40 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/40.wav'); //✓
+    window.a30 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/30.wav'); //✓
+    window.a20 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/20.wav'); //✓
+    window.a10 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/10.wav'); //✓
+    window.aRetard = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/retard.wav'); //✓
+    window.a5 = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/5.wav'); //✓
+    window.stall = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/stall.wav'); //✓
+    window.glideslope = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/glideslope.wav'); //✓
+    window.tooLowFlaps = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/too-low_flaps.wav'); //✓
+    window.tooLowGear = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/too-low_gear.wav'); //✓
+    window.apDisconnect = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/ap-disconnect.wav'); //✓
+    window.minimumBaro = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/minimum.wav'); //✓
+    window.dontSink = new Audio('https://tylerbmusic.github.io/GPWS-files_geofs/dont-sink.wav'); //✓
+    window.iminimums = false;
+    window.i2500 = false;
+    window.i2000 = false;
+    window.i1000 = false;
+    window.i500 = false;
+    window.i400 = false;
+    window.i300 = false;
+    window.i200 = false;
+    window.i100 = false;
+    window.i50 = false;
+    window.i40 = false;
+    window.i30 = false;
+    window.i20 = false;
+    window.i10 = false;
+    window.i7 = false;
+    window.i5 = false;
+    window.gpwsRefreshRate = 100;
+    function isInRange(i, a, vs) {
+        if (i >= 100) {
+            if ((i <= a+10) && (i >= a-10)) {
+                return true;
+            }
+        } else if (i >= 10) {
+            if ((i < a+4) && (i > a-4)) {
+                return true;
+            }
+        } else {
+            if (i <= a+1 && i >= a-1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    window.wasAPOn = false;
+    window.isRadioPanelOpen = false;
+    function updateGPWS() {
+        // Check if geofs.animation.values is available
+        if (typeof geofs.animation.values != 'undefined' && !geofs.isPaused()) {
+            var RadioPanel = document.getElementsByClassName("geofs-radio-controls");
+            if (document.getElementById("minimums") == null && !window.isRadioPanelOpen) {
+                RadioPanel[0].innerHTML += `
+                <div class="geofs-radio-control geofs-minimums">
+                <span class="geofs-radio-label">BARO (Minimums)</span>
+                <input class="geofs-radio-display" id="minimums" name="minimums">`;
+                window.isRadioPanelOpen = true;
+            } else if (document.getElementById("minimums") !== null) {
+                window.isRadioPanelOpen = false;
+            }
+            // Retrieve and format the required values
+            var minimum = ((document.getElementById("minimums") !== null) && document.getElementById("minimums").value !== undefined) ? Number(document.getElementById("minimums").value) : undefined;
+            var agl = (geofs.animation.values.altitude !== undefined && geofs.animation.values.groundElevationFeet !== undefined) ? Math.round((geofs.animation.values.altitude - geofs.animation.values.groundElevationFeet) + (geofs.aircraft.instance.collisionPoints[geofs.aircraft.instance.collisionPoints.length - 2].worldPosition[2]*3.2808399)) : 'N/A';
+            var verticalSpeed = geofs.animation.values.verticalSpeed !== undefined ? Math.round(geofs.animation.values.verticalSpeed) : 'N/A';
+            //Glideslope calculation
+            var glideSlope;
+                if (geofs.animation.getValue("NAV1Direction") && (geofs.animation.getValue("NAV1Distance") !== 600)) { //The second part to the if statement prevents the divide by 0 error.
+                    glideSlope = (geofs.animation.getValue("NAV1Direction") === "to") ? (Math.atan((agl*0.3048) / (geofs.animation.getValue("NAV1Distance")+600))*RAD_TO_DEGREES).toFixed(1) : (Math.atan((agl*0.3048) / Math.abs(geofs.animation.getValue("NAV1Distance")-600))*RAD_TO_DEGREES).toFixed(1);
+                } else {
+                    glideSlope = undefined;
+                } //End Glideslope calculation
+            if (geofs.aircraft.instance.stalling && !geofs.aircraft.instance.groundContact && window.stall.paused) { //Stall
+                window.stall.play();
+            }
+            if (agl > 100 && (glideSlope < 1.5 || glideSlope > 5) && window.glideslope.paused) { //Glideslope
+                window.glideslope.play();
+            }
+            if (!geofs.aircraft.instance.groundContact && agl < 300 && (geofs.aircraft.instance.definition.gearTravelTime !== undefined) && (geofs.animation.values.gearPosition >= 0.5) && window.tooLowGear.paused) { //Too Low - Gear (This warning takes priority over the Too Low - Flaps warning)
+                window.tooLowGear.play();
+            }
+            if (!geofs.aircraft.instance.groundContact && agl < 500 && (geofs.animation.values.flapsSteps !== undefined) && (geofs.animation.values.flapsPosition == 0) && window.tooLowGear.paused && window.tooLowFlaps.paused) { //Too Low - Flaps
+                window.tooLowFlaps.play();
+            }
+            if (!geofs.autopilot.on && window.wasAPOn) { //Autopilot Disconnect
+                    window.apDisconnect.play();
+                }
+            if (verticalSpeed <= 0) {
+                if (!geofs.aircraft.instance.groundContact && agl < 300 && geofs.animation.values.throttle > 0.95 && window.dontSink.paused) { //Don't Sink
+                    window.dontSink.play();
+                }
+                if ((minimum !== undefined) && (geofs.animation.values.altitude+2 > minimum && minimum > geofs.animation.values.altitude-2) && !window.iminimums) { //Minimum
+                    window.minimumBaro.play();
+                    window.iminimums = true;
+                }
+                if (isInRange(2500, agl) && !window.i2500) { //2,500
+                    window.a2500.play();
+                    window.i2500 = true;
+                }
+                if (isInRange(2000, agl) && !window.i2000) { //2,000
+                    window.a2000.play();
+                    window.i2000 = true;
+                }
+                if (isInRange(1000, agl) && !window.i1000) { //1,000
+                    window.a1000.play();
+                    window.i1000 = true;
+                }
+                if (isInRange(500, agl) && !window.i500) { //500
+                    window.a500.play();
+                    window.i500 = true;
+                }
+                if (isInRange(400, agl) && !window.i400) { //400
+                    window.a400.play();
+                    window.i400 = true;
+                }
+                if (isInRange(300, agl) && !window.i300) { //300
+                    window.a300.play();
+                    window.i300 = true;
+                }
+                if (isInRange(200, agl) && !window.i200) { //200
+                    window.a200.play();
+                    window.i200 = true;
+                }
+                if (isInRange(100, agl) && !window.i100) { //100
+                    window.a100.play();
+                    window.i100 = true;
+                }
+                if (isInRange(50, agl) && !window.i50) { //50
+                    window.a50.play();
+                    window.i50 = true;
+                }
+                if (isInRange(40, agl) && !window.i40) { //40
+                    window.a40.play();
+                    window.i40 = true;
+                }
+                if (isInRange(30, agl) && !window.i30) { //30
+                    window.a30.play();
+                    window.i30 = true;
+                }
+                if (isInRange(20, agl) && !window.i20) { //20
+                    window.a20.play();
+                    window.i20 = true;
+                }
+                if (isInRange(10, agl) && !window.i10) { //10
+                    window.a10.play();
+                    window.i10 = true;
+                }
+                if (!geofs.aircraft.instance.groundContact && ((agl+(geofs.animation.values.verticalSpeed/60)*2) <= 1.0) && !window.i7) { //Retard 2 seconds from touchdown
+                    window.aRetard.play();
+                    window.i7 = true;
+                }
+                if (isInRange(5, agl) && !window.i5) { //5
+                    window.a5.play();
+                    window.i5 = true;
+                }
+                window.gpwsRefreshRate = 30;
+            } else if (verticalSpeed > 0) {
+                if (window.iminimums) {
+                    window.iminimums = false;
+                }
+                if (window.i2500) {
+                    window.i2500 = false;
+                }
+                if (window.i2000) {
+                    window.i2000 = false;
+                }
+                if (window.i1000) {
+                    window.i1000 = false;
+                }
+                if (window.i500) {
+                    window.i500 = false;
+                }
+                if (window.i400) {
+                    window.i400 = false;
+                }
+                if (window.i300) {
+                    window.i300 = false;
+                }
+                if (window.i200) {
+                    window.i200 = false;
+                }
+                if (window.i100) {
+                    window.i100 = false;
+                }
+                if (window.i50) {
+                    window.i50 = false;
+                }
+                if (window.i40) {
+                    window.i40 = false;
+                }
+                if (window.i30) {
+                    window.i30 = false;
+                }
+                if (window.i20) {
+                    window.i20 = false;
+                }
+                if (window.i10) {
+                    window.i10 = false;
+                }
+                if (window.i7) {
+                    window.i7 = false;
+                }
+                if (window.i5) {
+                    window.i5 = false;
+                }
+                window.gpwsRefreshRate = 100;
+            }
+        }
+        window.wasAPOn = geofs.autopilot.on;
+    }
+
+    // Update flight data display every 100ms
+    setInterval(updateGPWS, window.gpwsRefreshRate);
+})();
